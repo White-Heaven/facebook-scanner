961720d966ffb704fcb9112c2e7f464db9a99526
DArkHeaveN <65353899+White-Heaven@users.noreply.github.com>
2021-01-06 00:13:11 +0530

    Update facebookhack.py

--- facebookhack.py
+++ facebookhack.py
@@ -15,7 +15,7 @@ print "     ) _)  ) _ ((___)) _ ( )   /) \/ (  )(   ) _)  "
 print "    (__)  (____/    (____/(__\_)\____/ (__) (____) "
 
 print ("-"*58)
-print "  v1.0 coded by darkheaven
+print "  v1.0 coded by darkheaven "
 print ("-"*58)
 print "\n"
 
